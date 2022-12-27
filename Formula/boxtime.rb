979efc172da47f17ba5341c6e3b5e8109a91e566
Tom Hombergs <tom.hombergs@gmail.com>
2022-12-27 22:08:38 +1100

    0.0.2

--- Formula/boxtime.rb
+++ Formula/boxtime.rb
@@ -2,7 +2,7 @@ class Boxtime < Formula
   desc "Take control of your time by tracking your time-spending habits."
   homepage "https://github.com/thombergs/boxtime-cli"
   url "https://github.com/thombergs/boxtime-cli/releases/download/v0.0.2/boxtime-macos-latest.tar.gz"
-  sha256 "12b6742db244be2113b3f42011572c23823d67dee173eb709a021c03f54172ac"
+  sha256 "7d56073d11763f81b731947b8f46b75129e5eee8f142529314234e6854b4a799"
   license "MIT"
   version "0.0.2"
 
