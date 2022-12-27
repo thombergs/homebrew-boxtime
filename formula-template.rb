a828fe57b72b80ac8aa052d5753b98e6021779d7
Tom Hombergs <tom.hombergs@gmail.com>
2022-12-27 23:27:15 +1100

    Create formula-template.rb

---
+++ formula-template.rb
@@ -0,0 +1,12 @@
+class Boxtime < Formula
+  desc "Take control of your time by tracking your time-spending habits."
+  homepage "https://github.com/thombergs/boxtime-cli"
+  url "https://github.com/thombergs/boxtime-cli/releases/download/@@TAG@@/boxtime-macos-latest.tar.gz"
+  sha256 "@@SHA@@"
+  license "MIT"
+  version "@@TAG@@"
+
+  def install
+    bin.install "boxtime"
+  end
+end
