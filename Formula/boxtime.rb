fd67447eb0149a9e6fd0f1a38a35994e7d8b9490
thombergs <tom.hombergs@gmail.com>
2022-12-29 03:14:07 +0000

    Update Homewbrew formula to 0.0.5

--- Formula/boxtime.rb
+++ Formula/boxtime.rb
@@ -1,10 +1,10 @@
 class Boxtime < Formula
   desc "Take control of your time by tracking your time-spending habits."
   homepage "https://github.com/thombergs/boxtime-cli"
-  url "https://github.com/thombergs/boxtime-cli/releases/download/v0.0.4/boxtime-macos-latest.tar.gz"
-  sha256 "39c79ed4beeb1fe4a1a9d73db857470d3f9ba5ac3cd6e730fe6bdd83e836515f"
+  url "https://github.com/thombergs/boxtime-cli/releases/download/v0.0.5/boxtime-macos-latest.tar.gz"
+  sha256 "9e809f2cb895f9e8c65832c148d0b1ff71de8c44d72e4a4ae5bcfd62a6b93b52"
   license "MIT"
-  version "0.0.4"
+  version "0.0.5"
 
   def install
     bin.install "boxtime"
