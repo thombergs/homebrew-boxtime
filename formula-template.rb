085f7e3aa9055cfa4f685526663f2bc10491de09
Tom Hombergs <tom.hombergs@gmail.com>
2023-01-10 10:48:55 +1100

    Update formula-template.rb

--- formula-template.rb
+++ formula-template.rb
@@ -1,7 +1,7 @@
 class Boxtime < Formula
   desc "Take control of your time by tracking your time-spending habits."
   homepage "https://github.com/thombergs/boxtime-cli"
-  url "https://github.com/thombergs/boxtime-cli/releases/download/v@@TAG@@/boxtime-macos-latest.tar.gz"
+  url "https://github.com/thombergs/boxtime-cli/releases/download/v@@TAG@@/boxtime-macOS.tar.gz"
   sha256 "@@SHA@@"
   license "MIT"
   version "@@TAG@@"
